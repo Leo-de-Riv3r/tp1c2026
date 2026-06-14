f32faa5a475abe8920948bfc82128de3442755dc
Salomé Tredici <salome.tredici@outlook.com>
2026-06-14 03:52:56 -0300

    Log in run to debug deploy

--- backend/src/main/java/com/tacs/tp1c2026/Tp1c2026Application.java
+++ backend/src/main/java/com/tacs/tp1c2026/Tp1c2026Application.java
@@ -6,6 +6,8 @@ import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
@@ -15,5 +17,8 @@ public class Tp1c2026Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Tp1c2026Application.class, args);
 	}
-
+	@PostConstruct
+	public void checkEnv() {
+    	System.out.println("SPRING_MONGODB_URI=" + System.getenv("SPRING_MONGODB_URI"));
+	}
 }
